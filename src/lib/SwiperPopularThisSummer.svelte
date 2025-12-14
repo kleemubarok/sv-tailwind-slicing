cbc5c7bb51b0ecf91cd3267744c8c67fd9f58903
Barikly Mubarok <xiomay@Xiomay.local>
2025-12-14 15:38:40 +0700

    2.3 nav buttom

---
+++ src/lib/SwiperPopularThisSummer.svelte
@@ -0,0 +1,61 @@
+<script>
+	/* Swiper 11 + Svelte 5 */
+	import { register } from 'swiper/element/bundle'; // web-component
+	import { resolve } from '$app/paths';
+	register(); // daftarkan <swiper-container>
+
+	// props
+	let { items = [], class: klass = '' } = $props();
+</script>
+
+<swiper-container class="swiper-wrapper {klass}" slides-per-view="auto" space-between="20">
+	{#each items as item (item.id)}
+		<swiper-slide class="w-fit!">
+			<a href={resolve(item.href)} class="card">
+				<div
+					class="relative flex w-58 shrink-0 flex-col gap-3 overflow-hidden rounded-3xl border border-shujia-graylight bg-white p-4 transition-all duration-300 hover:border-shujia-orange"
+				>
+					<span class="absolute right-6.5 top-6.5 shrink-0 rounded-full bg-white px-2 py-1.75">
+						<div class="flex items-center gap-0.5">
+							<img src="assets/images/icons/star-rating.svg" alt="star-rating" />
+							<p class="text-xs font-semibold leading-4.5">{item.rating}</p>
+						</div>
+					</span>
+					<div
+						class="flex h-35 w-full shrink-0 items-center justify-center overflow-hidden rounded-2xl bg-[#D9D9D9]"
+					>
+						<img src={item.img} alt="Popular Items" />
+					</div>
+					<h3 class="line-clamp-2 min-h-[48px] font-semibold">{item.title}</h3>
+					<div class="flex flex-col gap-y-3">
+						<div class="flex items-center gap-2">
+							<img
+								class="h-5 w-5 shrink-0"
+								src="assets/images/icons/calendar.svg"
+								alt="romm name icon"
+							/>
+							<p class="text-sm leading-5.25 text-shujia-gray">{item.room_name}</p>
+						</div>
+						<div class="flex items-center gap-2">
+							<img class="h-5 w-5 shrink-0" src="assets/images/icons/clock.svg" alt="time icon" />
+							<p class="text-sm leading-5.25 text-shujia-gray">{item.time}</p>
+						</div>
+						<strong class="font-semibold text-shujia-orange">{item.price}</strong>
+						<img
+							class="absolute bottom-0 right-0"
+							src="assets/images/backgrounds/crown.svg"
+							alt="crown icon"
+						/>
+					</div>
+				</div>
+			</a>
+		</swiper-slide>
+	{/each}
+</swiper-container>
+
+<style>
+	/* tambahan kecil bila perlu */
+	swiper-container::part(container) {
+		padding-bottom: 4px; /* agar shadow tidak terpotong */
+	}
+</style>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { resolve } from '$app/paths';
 	import SwiperCategory from '$lib/SwiperCategory.svelte';
+	import PopularThisSummer from '$lib/SwiperPopularThisSummer.svelte';
 
 	const categories = [
 		{
@@ -28,6 +29,27 @@
 			count: 745
 		}
 		// tambah lagi …
+	];
+
+	const popular_items = [
+		{
+			href: resolve('category/kitchen'),
+			rating: '4.8',
+			img: 'https://picsum.photos/id/1/200/300',
+			title: 'Home Family Theater Best Lights Installation',
+			room_name: 'Living Room',
+			time: '15 Hours',
+			price: 'Rp 8.560.392'
+		},
+		{
+			href: resolve('category/kitchen'),
+			rating: '4.8',
+			img: 'https://picsum.photos/id/20/200/300',
+			title: 'Rumah Kaca',
+			room_name: 'Garden ',
+			time: '15 Hours',
+			price: 'Rp 6.560.392'
+		}
 	];
 </script>
 
@@ -74,7 +96,64 @@
 	<section id="categories" class="swiper w-full overflow-x-hidden">
 		<SwiperCategory items={categories} />
 	</section>
-	<section id="adverticement"></section>
-	<section id="popularsummer"></section>
+	<section id="adverticement" class="px-5">
+		<a href="#">
+			<img src="assets/images/backgrounds/ads.png" alt="icon" />
+		</a>
+	</section>
+	<section id="popularsummer" class="space-y-3.5">
+		<h2 class="pl-5 tex-xl font-bold leading-7">Popular This Summer</h2>
+		<div id="PopularSummerSlider" class="px-5 swiper w-full overflow-x-hidden">
+			<PopularThisSummer items={popular_items} />
+		</div>
+	</section>
 </div>
-<nav id="navbuttom"></nav>
+<nav id="navbuttom" class="fixed bottom-5 left-0 right-0 z-30 mx-auto w-full">
+	<div class="mx-auto max-w-160 px-5">
+		<div class="rounded-3xl bg-shujia-black px-5 py-3.5">
+			<ul class="flex items-center gap-[20.30px]">
+				<li class="w-full">
+					<a href="#">
+						<div
+							class="flex items-center justify-center gap-2 rounded-full bg-shujia-orange px-[18px] py-[10px] transition-all duration-300 hover:shadow-[0px_4px_10px_0px_#D04B1E80]"
+						>
+							<img
+								class="h-6 w-6 shrink-0"
+								src="assets/images/icons/calendar-gemini.svg"
+								alt="icon"
+							/>
+							<p class="text-sm font-semibold leading-[21px] text-white">Browse</p>
+						</div>
+					</a>
+				</li>
+				<li class="shrink-0">
+					<a href="#">
+						<div
+							class="flex h-[44px] w-[44px] shrink-0 items-center justify-center rounded-full border border-shujia-graylight transition-all duration-300 hover:border-shujia-orange"
+						>
+							<img src="assets/images/icons/toilet-paper.svg" alt="icon" />
+						</div>
+					</a>
+				</li>
+				<li class="shrink-0">
+					<a href="#">
+						<div
+							class="flex h-[44px] w-[44px] shrink-0 items-center justify-center rounded-full border border-shujia-graylight transition-all duration-300 hover:border-shujia-orange"
+						>
+							<img src="assets/images/icons/chat.svg" alt="icon" />
+						</div>
+					</a>
+				</li>
+				<li class="shrink-0">
+					<a href="#">
+						<div
+							class="flex h-[44px] w-[44px] shrink-0 items-center justify-center rounded-full border border-shujia-graylight transition-all duration-300 hover:border-shujia-orange"
+						>
+							<img src="assets/images/icons/profile.svg" alt="icon" />
+						</div>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
